8c166b55de4973eeae39b6fe0ea7ad1af9d03f1b
unknown <you@example.com>
2020-11-24 18:17:42 +0530

    Added New File

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,8 +1,25 @@
 #!/bin/bash -x
+ct=0
+ct1=0
+ct2=0
+for ((i=1;$i<11;i++))
+do
 Toss=$(($RANDOM%2))
+ct=$(($ct + 1))
 if [ $Toss -eq 0 ]
 then
-	echo "++++HEAD+++"
+
+	ct1=$(($ct1 + 1))
+	per1=$(( $ct1/$ct * 100 ))
 else
-	echo "+++TAIL+++"
+
+	ct2=$(($ct2 + 1))
+	per2=$(( $ct2/$ct * 100 ))
 fi
+done
+echo "number of Times Head :$ct1"
+echo "number of Times tail :$ct2"
+arr[0]="HEAD:$per1"
+arr[1]="TAIL:$per2"
+echo ""
+echo "${arr[@]}"
