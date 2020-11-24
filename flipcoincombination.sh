a9cde304b55e5846b1fd38c7d23f6893636f9c63
unknown <you@example.com>
2020-11-24 18:53:38 +0530

    Added new File

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -4,9 +4,13 @@ ct1=0
 ct2=0
 ct3=0
 ct4=0
-for ((i=1;$i<11;i++))
+ct5=0
+ct6=0
+ct7=0
+ct8=0
+for ((i=1;$i<20;i++))
 do
-Toss=$(($RANDOM%4))
+Toss=$(($RANDOM%8))
 ct=$(($ct + 1))
 if [ $Toss -eq 0 ]
 then
@@ -26,15 +30,44 @@ elif [ $Toss -eq 3 ]
 then
 	ct4=$(($ct4 + 1 ))
 	per4=$(($ct4/$ct * 100))
+elif  [ $Toss -eq 4 ]
+then
+        ct5=$(($ct5 + 1))
+        per5=$(( $ct5/$ct * 100 ))
+elif  [ $Toss -eq 5 ]
+then
+        ct6=$(($ct6 + 1))
+        per6=$(( $ct6/$ct * 100 ))
+
+
+elif  [ $Toss -eq 6 ]
+then
+        ct7=$(($ct7 + 1))
+        per7=$(( $ct7/$ct * 100 ))
+
+elif  [ $Toss -eq 7 ]
+then
+        ct8=$(($ct8 + 1))
+        per8=$(( $ct8/$ct * 100 ))
 fi
 done
-echo "number of Times HH :$ct1"
-echo "number of Times HT :$ct2"
-echo "number of Times TH : $ct3"
-echo "number of Times TT : $ct4"
-arr[0]="HH:$per1"
-arr[1]="HT:$per2"
-arr[2]="TH:$per3"
-arr[3]="TT:$per4"
+echo "number of times HHH comes: $ct1"
+echo "number of times HHT comes: $ct2"
+echo "number of times HTH comes: $ct3"
+echo "number of times HTT comes: $ct4"
+echo "number of times THH comes: $ct5"
+echo "number of times THT comes: $ct6"
+echo "number of times TTH comes: $ct7"
+echo "number of times TTT comes: $ct8"
+arr[0]="HHH:$per1 : "
+arr[1]="HHT:$per2 : "
+arr[2]="HTH:$per3 : "
+arr[3]="HTT:$per4 : "
+arr[4]="THH:$per5 : "
+arr[5]="THT:$per6 : "
+arr[6]="TTH:$per7 : "
+arr[7]="TTT:$per8 : "
+
 echo ""
 echo "${arr[@]}"
+
